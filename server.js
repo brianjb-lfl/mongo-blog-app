7d02560e666db8f3541fcb3456aa0321747ae41e
Brian Besaw <bjbesaw@outlook.com>
2017-09-19 19:10:22 -0400

    CRUD functions working

--- server.js
+++ server.js
@@ -29,39 +29,77 @@ app.get('/posts', (req, res) => {
       });
 });
 
-
 app.get('/posts/:id', (req, res) => {
-
-  
-
+  Post
+    .findById(req.params.id)
+    .then(post => res.json(post.apiRepr()))
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({message: 'Internal server error'});
+    });
 });
 
 
 app.post('/posts', (req, res) => {
-
+  const reqFields = ['title', 'content', 'author'];
+  for (let i = 0; i < reqFields.length; i++){
+    const field = reqFields[i];
+    if(!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`;
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
   
-
+  Post
+    .create({
+      title: req.body.title,
+      content: req.body.content,
+      author: req.body.author
+    })
+    .then(
+      post => res.status(201).json(post.apiRepr())            // should have location header
+    )
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({message: 'Internal server error'});
+    });
 });
 
-
 app.put('/posts/:id', (req, res) => {
+  if(!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+    const message = `Request header id ${req.params.id} and 
+      body id ${req.body.id} mismatch`;
+    console.error(message);
+    return res.status(400).send(message);
+  }
 
-
-
+  const updateableFields = ['title', 'content', 'author'];
+  let updateObj = {};
+  updateableFields.forEach( field => {
+    if(field in req.body){
+      updateObj[field] = req.body[field];
+    }
+    console.log(updateObj);
+  });  
+  Post
+    .findByIdAndUpdate(req.params.id, {$set: updateObj} )
+    .then(res.sendStatus(204))
+    .catch(err => res.status(500).json({message: 'Internal server error'}));
 });
 
 
 app.delete('/posts/:id', (req, res) => {
-
-
+  console.log(req.params.id);
+  Post
+    .findByIdAndRemove(req.params.id)
+    .then(res.sendStatus(204))
+    .catch(err => res.status(500).json({message: 'Internal server error'}));  
 });
 
 
-
-
-
 app.use('*', function(req, res) {
-  res.status(400).json({message: 'endpoint not found'});
+  res.status(404).json({message: 'endpoint not found'});
 });
 
 let server;
@@ -77,8 +115,7 @@ function runServer(databaseUrl=DATABASE_URL, port=PORT) {
       server = app.listen(port, () => {
         console.log(`Your app is listening on port ${port}`);
         resolve();
-      })
-      .on('error', err => {
+      }).on('error', err => {
         mongoose.disconnect();
         reject(err);
       });
@@ -90,15 +127,15 @@ function runServer(databaseUrl=DATABASE_URL, port=PORT) {
 // use it in our integration tests later.
 function closeServer() {
   return mongoose.disconnect().then(() => {
-     return new Promise((resolve, reject) => {
-       console.log('Closing server');
-       server.close(err => {
-           if (err) {
-               return reject(err);
-           }
-           resolve();
-       });
-     });
+    return new Promise((resolve, reject) => {
+      console.log('Closing server');
+      server.close(err => {
+        if (err) {
+          return reject(err);
+        }
+        resolve();
+      });
+    });
   });
 }
 
@@ -106,6 +143,6 @@ function closeServer() {
 // runs. but we also export the runServer command so other code (for instance, test code) can start the server as needed.
 if (require.main === module) {
   runServer().catch(err => console.error(err));
-};
+}
 
 module.exports = {app, runServer, closeServer};
